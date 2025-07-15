9dd9493c4d4f70a55c63112f31d1add25eb66dd9
muskan <asudanim.21@gmail.com>
2025-07-15 22:20:55 +0530

    made some classes

--- playground/views.py
+++ playground/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+def calculate():
+    x = 1
+    y = 2
+    return x 
+
 def say_hello(request):
-    return HttpResponse('Hello, World!')
+    #return HttpResponse('Hello, World!')
+    x=calculate()
+    print(x)
+    return render(request,'hello.html',{'name':'Muskan'})
+
+def home(request):
+    return HttpResponse('Welcome to the Home Page!')
